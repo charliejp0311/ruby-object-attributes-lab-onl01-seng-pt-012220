5ead2c39e7c99f82f438463643676201bdfa451e
Charles Pelton <charliejp0311@gmail.com>
2020-01-30 14:42:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
-class Dog 
+class Dog
   def name=(dogs_name)
     @name = persons_name
   end
-  def name 
+  def name
     @name
   end
   def breed=(dogs_breed)
     @breed = dogs_breed
   end
-  def breed 
-    @breed 
+  def breed
+    @breed
   end
 end
